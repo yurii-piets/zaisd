9a1015c1606a1f8ae32dd5d1855689368907da4d
Yurii Piets <yurii.piets@gmail.com>
2019-10-11 10:06:32 +0200

    lab2 | add file reader

--- lab2/run.py
+++ lab2/run.py
@@ -1,4 +1,18 @@
+import csv
+
+from common.point import Point
+
+
+def read_points(file_name):
+    points = []
+    with open(file_name, encoding="mbcs") as csv_file:
+        rows = csv.reader(csv_file, delimiter=';')
+        for row in rows:
+            if len(row) != 0:
+                points.append(Point(row[0], row[1]))
+    return points
 
 
 if __name__ == '__main__':
-    all_points = read_points("punktyPrzykladowe.csv")
+    all_points = read_points("punkty.csv")
+    print(all_points)
